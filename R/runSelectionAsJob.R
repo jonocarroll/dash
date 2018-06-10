260cc909fadd69aa6dfa779a829eaabfd097592b
Jonathan Carroll <jono@jcarroll.com.au>
2018-06-11 04:16:28 +0930

    addin to run selection as Job in background

---
+++ R/runSelectionAsJob.R
@@ -0,0 +1,18 @@
+runSelectionAsJob <- function() {
+
+  ## Get the document context.
+  context <- rstudioapi::getActiveDocumentContext()$selection[[1]]$text
+
+  ## save selection to a temporary file
+  tf <- tempfile("dashJob", fileext = ".R")
+  writeLines(context, con = tf, sep = "\n")
+
+  ## run the temp script as a job, returning the
+  ## results to the global environment
+  .rs.api.runScriptJob(
+    path = path.expand(tf),
+    workingDir = path.expand(tempdir()),
+    exportEnv = "R_GlobalEnv"
+  )
+
+}
